82cf4733272065d8ffab59a5e7d250281776a8c9
shutupno <970536094@qq.com>
2019-04-22 00:39:43 +0800

    添加sass

--- first-vue/src/App.vue
+++ first-vue/src/App.vue
@@ -2,7 +2,11 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
-    <div>sfsdfsf</div>
+    <div class="box">
+      <div class="bg">
+        随便测试用的
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,7 +16,11 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+$browser-default-font-size: 100px !default; //根据自己的需求设定大小
+@function pxTorem($px) {//$px为需要转换的字号
+  @return $px / $browser-default-font-size * 1rem;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -20,5 +28,23 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.box {
+  width: pxTorem(1250px);
+  height: 200px;
+  background-color: beige;
+  margin: 0 auto;
+
+  .bg {
+    background-color: #2c3e50;
+    width: 100px;
+    line-height: 100px;
+    margin: 0 auto;
+    text-align: center;
+    display: inline-block;
+    color: antiquewhite;
+    font-size: pxTorem(14px);
+  }
 }
 </style>
