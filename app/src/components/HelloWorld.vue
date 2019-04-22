e1f3571d404a8d5d5ec4b32be532dc22f8070270
shutupno <970536094@qq.com>
2019-04-23 02:04:32 +0800

    app

---
+++ app/src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <div class="box">
+      <div class="ch">
+        这个是测试sass的
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+  .box {
+    width: 300px;
+    height: 300px;
+    background-color: #efefef;
+
+    .ch {
+      font-size: 20px;
+      color: #42b983;
+    }
+  }
+</style>
